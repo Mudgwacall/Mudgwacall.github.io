43705bd53a71e09282e8aae8d26bed05450f6664
Mudgwacall <cmudgway@hotmail.co.nz>
2014-10-15 14:15:01 +1300

    d

--- WorldPerspectives/main.js
+++ WorldPerspectives/main.js
@@ -9,10 +9,11 @@
 		  Twitch.init({clientId: 'hj0u0ban55ryh4hs4gv6q18u70gohm6'}, function(error, status) {
 			  
 			  console.log(status);
+			  $('video').hide();
 			  
 			  if(status.authenticated){
  				  $('.twitch-connect').hide();
-				 
+				  $('video').show();
 				  getInfo(function(data){
 					  $('strong').text(data.display_name);
 					  $('#picture').attr('src','http://static-cdn.jtvnw.net/jtv_user_pictures/imdonning-profile_image-dc549200c02cbf4a-150x150.png');

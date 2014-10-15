054a4911e7e9dc51870e30926458bf5e245d946f
Mudgwacall <cmudgway@hotmail.co.nz>
2014-10-15 14:16:03 +1300

    d

--- WorldPerspectives/main.js
+++ WorldPerspectives/main.js
@@ -5,11 +5,11 @@
 					console.log("test log");
 				});
 			}
-			
+			$('video').hide();
 		  Twitch.init({clientId: 'hj0u0ban55ryh4hs4gv6q18u70gohm6'}, function(error, status) {
 			  
 			  console.log(status);
-			  $('video').hide();
+			  
 			  
 			  if(status.authenticated){
  				  $('.twitch-connect').hide();
